3e7defa1e2821a3146f63a868ee036b66c6bcd08
VladPiatachenko <vlpiatachenko@gmail.com>
2022-09-20 21:40:47 +0300

    prototype pattern

--- Square.java
+++ Square.java
@@ -1,2 +1,11 @@
-public class Square {
+public class Square extends Shape {
+
+    public Square(){
+        type = "Square";
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Square::draw() method.");
+    }
 }
